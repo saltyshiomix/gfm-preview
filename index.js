45bf87d4624b117fe28fd39cb27947a8a07a58ac
Yoshihide Shiono <shiono.yoshihide@gmail.com>
2018-08-01 23:51:49 +0900

    Implement preview command

--- index.js
+++ index.js
@@ -1,101 +1,75 @@
 #!/usr/bin/env node
-"use strict";
-var __awaiter = (this && this.__awaiter) || function (thisArg, _arguments, P, generator) {
-    return new (P || (P = Promise))(function (resolve, reject) {
-        function fulfilled(value) { try { step(generator.next(value)); } catch (e) { reject(e); } }
-        function rejected(value) { try { step(generator["throw"](value)); } catch (e) { reject(e); } }
-        function step(result) { result.done ? resolve(result.value) : new P(function (resolve) { resolve(result.value); }).then(fulfilled, rejected); }
-        step((generator = generator.apply(thisArg, _arguments || [])).next());
-    });
-};
-var __generator = (this && this.__generator) || function (thisArg, body) {
-    var _ = { label: 0, sent: function() { if (t[0] & 1) throw t[1]; return t[1]; }, trys: [], ops: [] }, f, y, t, g;
-    return g = { next: verb(0), "throw": verb(1), "return": verb(2) }, typeof Symbol === "function" && (g[Symbol.iterator] = function() { return this; }), g;
-    function verb(n) { return function (v) { return step([n, v]); }; }
-    function step(op) {
-        if (f) throw new TypeError("Generator is already executing.");
-        while (_) try {
-            if (f = 1, y && (t = op[0] & 2 ? y["return"] : op[0] ? y["throw"] || ((t = y["return"]) && t.call(y), 0) : y.next) && !(t = t.call(y, op[1])).done) return t;
-            if (y = 0, t) op = [op[0] & 2, t.value];
-            switch (op[0]) {
-                case 0: case 1: t = op; break;
-                case 4: _.label++; return { value: op[1], done: false };
-                case 5: _.label++; y = op[1]; op = [0]; continue;
-                case 7: op = _.ops.pop(); _.trys.pop(); continue;
-                default:
-                    if (!(t = _.trys, t = t.length > 0 && t[t.length - 1]) && (op[0] === 6 || op[0] === 2)) { _ = 0; continue; }
-                    if (op[0] === 3 && (!t || (op[1] > t[0] && op[1] < t[3]))) { _.label = op[1]; break; }
-                    if (op[0] === 6 && _.label < t[1]) { _.label = t[1]; t = op; break; }
-                    if (t && _.label < t[2]) { _.label = t[2]; _.ops.push(op); break; }
-                    if (t[2]) _.ops.pop();
-                    _.trys.pop(); continue;
-            }
-            op = body.call(thisArg, _);
-        } catch (e) { op = [6, e]; y = 0; } finally { f = t = 0; }
-        if (op[0] & 5) throw op[1]; return { value: op[0] ? op[1] : void 0, done: true };
-    }
-};
-var _this = this;
-exports.__esModule = true;
-var path_1 = require("path");
-var parseArgs = require("minimist");
-var fastify = require("fastify");
-var axios_1 = require("axios");
-var opn = require("opn");
-var argv = parseArgs(process.argv.slice(2), {
-    alias: {
-        v: 'version',
-        h: 'help'
-    },
-    boolean: [
-        'v',
-        'h'
-    ]
-});
+
+const { resolve } = require('path')
+const { existsSync, readFileSync } = require('fs')
+const parseArgs = require('minimist')
+
+const argv = parseArgs(process.argv.slice(2), {
+  alias: {
+    v: 'version',
+    h: 'help'
+  },
+  boolean: [
+    'v',
+    'h'
+  ]
+})
+
 if (argv.version) {
-    var pkg = require(path_1.resolve(__dirname, './package.json'));
-    console.log("gfm-preview v" + pkg.version);
-    process.exit(0);
+  const pkg = require(resolve(__dirname, './package.json'))
+  console.log(`gfm-preview v${pkg.version}`)
+  process.exit(0)
 }
-if (argv.help) {
-    console.log("\n    Description\n      Preview GitHub flavored markdown\n\n    Usage\n      $ gfm-preview <markdown file> [--github-api-url <github api url>]\n\n    Args\n      <markdown file>  A markdown file which you want to preview\n\n    Options\n      --github-api-url <github api url>  GitHub API URL (default: 'https://api.github.com')\n      --help, -h                         Displays this message\n  ");
-    process.exit(0);
+
+if (argv.help || (!process.argv[2])) {
+  console.log(`
+    Description
+      Preview GitHub flavored markdown
+
+    Usage
+      $ gfm-preview <markdown file> [--github-api-url <github api url>]
+
+    Args
+      <markdown file>  A markdown file which you want to preview
+
+    Options
+      --github-api-url <github api url>  GitHub API URL (default: 'https://api.github.com')
+      --help, -h                         Displays this message
+  `)
+  process.exit(0)
 }
-var port = 4649;
-var server = fastify();
-var start = function () { return __awaiter(_this, void 0, void 0, function () {
-    var err_1;
-    return __generator(this, function (_a) {
-        switch (_a.label) {
-            case 0:
-                _a.trys.push([0, 2, , 3]);
-                return [4 /*yield*/, server.listen(port)];
-            case 1:
-                _a.sent();
-                return [3 /*break*/, 3];
-            case 2:
-                err_1 = _a.sent();
-                server.log.error(err_1);
-                process.exit(1);
-                return [3 /*break*/, 3];
-            case 3: return [2 /*return*/];
-        }
-    });
-}); };
-var apiUrl = argv['github-api-url'] ? argv['github-api-url'] : 'https://api.github.com';
-server.get('/', function ( /*request, reply*/) { return __awaiter(_this, void 0, void 0, function () {
-    var response;
-    return __generator(this, function (_a) {
-        switch (_a.label) {
-            case 0: return [4 /*yield*/, axios_1["default"].post(apiUrl + '/markdown', {
-                    text: 'Hello world github/linguist#1 **cool**',
-                    mode: 'gfm'
-                })];
-            case 1:
-                response = _a.sent();
-                return [2 /*return*/, response.data];
-        }
-    });
-}); });
-opn("http://localhost:" + port);
-start();
+
+const file = resolve(process.argv[2])
+if (!existsSync(file)) {
+  console.error(`Not found: ${file}`)
+  process.exit(1)
+}
+
+const port = 4649
+const encoding = 'utf-8'
+const apiUrl = argv['github-api-url'] ? argv['github-api-url'] : 'https://api.github.com'
+const axios = require('axios')
+const fastify = require('fastify')()
+const start = async () => {
+  try {
+    await fastify.listen(port)
+  } catch (err) {
+    fastify.log.error(err)
+    process.exit(1)
+  }
+}
+
+fastify.get('/', async (request, reply) => {
+  const response = await axios.post(apiUrl + '/markdown', { text: readFileSync(file, encoding), mode: 'gfm' })
+  const content = response.data
+  reply.header('Content-Type', 'text/html; charset=' + encoding)
+  reply.send(readFileSync('./index.html', encoding).replace(/<!--TITLE-->/, process.argv[2]).replace(/<!--CONTENT-->/, content))
+})
+
+fastify.get('/hljs', async (request, reply) => {
+  reply.send(readFileSync('./hl.js', encoding))
+})
+
+require('opn')(`http://localhost:${port}`)
+
+start()
