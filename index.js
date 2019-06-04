60a5c8c7df572860c80f68efbe244c1f6eba0b93
Yoshihide Shiono <shiono.yoshihide@gmail.com>
2019-06-04 10:25:34 +0900

    Refactoring: code linting (manually)

--- index.js
+++ index.js
@@ -1,9 +1,16 @@
 #!/usr/bin/env node
 
-const { resolve, basename, extname } = require('path')
-const { existsSync, readFileSync } = require('fs')
-const chalk = require('chalk')
-const arg = require('arg')
+const {
+  existsSync,
+  readFileSync,
+} = require('fs');
+const {
+  resolve,
+  basename,
+  extname,
+} = require('path');
+const chalk = require('chalk');
+const arg = require('arg');
 
 const args = arg({
   '--help': Boolean,
@@ -16,16 +23,16 @@ const args = arg({
   '-v': '--version',
   '-b': '--browser',
   '-H': '--host',
-  '-p': '--port'
-})
+  '-p': '--port',
+});
 
 if (args['--version']) {
-  const pkg = require(resolve(__dirname, './package.json'))
-  console.log(`gfm-preview v${pkg.version}`)
-  process.exit(0)
+  const pkg = require(resolve(__dirname, './package.json'));
+  console.log(`gfm-preview v${pkg.version}`);
+  process.exit(0);
 }
 
-let filename = args._[0]
+let filename = args._[0];
 if (args['--help'] || (!filename)) {
   console.log(chalk`
     {bold.cyan gfm-preview} - Preview your markdown with GitHub API in real time
@@ -47,148 +54,148 @@ if (args['--help'] || (!filename)) {
       --host, -H {underline hostname}                      sets the hostname
       --port, -p {underline port_number}                   sets the port number
       --github-api-url {underline github_api_url}          sets the GitHub API URL (default: {underline https://api.github.com})
-  `)
-  process.exit(0)
+  `);
+  process.exit(0);
 }
 
-let currentFile = resolve(filename)
-let existsCurrentFile = existsSync(currentFile)
-const files = []
-const port = args['--port'] ? args['--port'] : 4649
-const encoding = 'utf-8'
-const apiUrl = args['--github-api-url'] ? args['--github-api-url'] : 'https://api.github.com'
-const axios = require('axios')
-const app = require('express')()
-const { watch } = require('chokidar')
-let watcher
+let currentFile = resolve(filename);
+let existsCurrentFile = existsSync(currentFile);
+const files = [];
+const port = args['--port'] ? args['--port'] : 4649;
+const encoding = 'utf-8';
+const apiUrl = args['--github-api-url'] ? args['--github-api-url'] : 'https://api.github.com';
+const axios = require('axios');
+const app = require('express')();
+const { watch } = require('chokidar');
+let watcher;
 
-app.get('/gfm/favicon.ico', async (_, res) => {
-  res.header('Content-Type', 'image/x-icon')
-  res.send(readFileSync(resolve(__dirname, 'favicon.ico')))
-})
+app.get('/gfm/favicon.ico', async (_req, res) => {
+  res.header('Content-Type', 'image/x-icon');
+  res.send(readFileSync(resolve(__dirname, 'favicon.ico')));
+});
 
-app.get('/gfm/favicon.png', async (_, res) => {
-  res.header('Content-Type', 'image/png')
-  res.send(readFileSync(resolve(__dirname, 'favicon.png')))
-})
+app.get('/gfm/favicon.png', async (_req, res) => {
+  res.header('Content-Type', 'image/png');
+  res.send(readFileSync(resolve(__dirname, 'favicon.png')));
+});
 
-app.get('/gfm/app.css', async (_, res) => {
-  res.header('Content-Type', 'text/css; charset=' + encoding)
-  res.send(readFileSync(resolve(__dirname, 'app.css'), encoding))
-})
+app.get('/gfm/app.css', async (_req, res) => {
+  res.header('Content-Type', 'text/css; charset=' + encoding);
+  res.send(readFileSync(resolve(__dirname, 'app.css'), encoding));
+});
 
-app.get('/gfm/hl.css', async (_, res) => {
-  res.header('Content-Type', 'text/css; charset=' + encoding)
-  res.send(readFileSync(resolve(__dirname, 'hl.css'), encoding))
-})
+app.get('/gfm/hl.css', async (_req, res) => {
+  res.header('Content-Type', 'text/css; charset=' + encoding);
+  res.send(readFileSync(resolve(__dirname, 'hl.css'), encoding));
+});
 
-app.get('/gfm/hl.js', async (_, res) => {
-  res.header('Content-Type', 'text/javascript; charset=' + encoding)
-  res.send(readFileSync(resolve(__dirname, 'hl.js'), encoding))
-})
+app.get('/gfm/hl.js', async (_req, res) => {
+  res.header('Content-Type', 'text/javascript; charset=' + encoding);
+  res.send(readFileSync(resolve(__dirname, 'hl.js'), encoding));
+});
 
-app.get('/gfm/socket.io.js', async (_, res) => {
-  res.header('Content-Type', 'text/javascript; charset=' + encoding)
-  res.send(readFileSync(resolve(__dirname, 'node_modules/socket.io-client/dist/socket.io.js'), encoding))
-})
+app.get('/gfm/socket.io.js', async (_req, res) => {
+  res.header('Content-Type', 'text/javascript; charset=' + encoding);
+  res.send(readFileSync(resolve(__dirname, 'node_modules/socket.io-client/dist/socket.io.js'), encoding));
+});
 
-app.get('/gfm/socket.io.js.map', async (_, res) => {
-  res.header('Content-Type', 'text/plain; charset=' + encoding)
-  res.send(readFileSync(resolve(__dirname, 'node_modules/socket.io-client/dist/socket.io.js.map'), encoding))
-})
+app.get('/gfm/socket.io.js.map', async (_req, res) => {
+  res.header('Content-Type', 'text/plain; charset=' + encoding);
+  res.send(readFileSync(resolve(__dirname, 'node_modules/socket.io-client/dist/socket.io.js.map'), encoding));
+});
 
 app.get('*', async (req, res) => {
   if (req.params[0] === '/') {
-    res.send('Please select a file to preview')
-    return
+    res.send('Please select a file to preview');
+    return;
   }
 
-  const file = resolve(req.params[0].slice(1))
-  currentFile = file
-  existsCurrentFile = existsSync(file)
+  const file = resolve(req.params[0].slice(1));
+  currentFile = file;
+  existsCurrentFile = existsSync(file);
   if (existsCurrentFile) {
     if (!files.includes(currentFile)) {
-      files.push(currentFile)
+      files.push(currentFile);
       if (watcher) {
-        watcher.close()
+        watcher.close();
       }
-      watcher = watch(files)
+      watcher = watch(files);
     }
-    res.header('Content-Type', 'text/html; charset=' + encoding)
-    let html = readFileSync(resolve(__dirname, 'index.html'), encoding)
-    html = html.replace(/{%TITLE%}/, basename(currentFile)).replace(/{%HOST%}/g, host).replace(/{%PORT%}/g, port)
-    res.send(html)
+    res.header('Content-Type', 'text/html; charset=' + encoding);
+    let html = readFileSync(resolve(__dirname, 'index.html'), encoding);
+    html = html.replace(/{%TITLE%}/, basename(currentFile)).replace(/{%HOST%}/g, host).replace(/{%PORT%}/g, port);
+    res.send(html);
   } else {
-    res.header('Content-Type', 'text/html; charset=' + encoding)
-    let html = readFileSync(resolve(__dirname, 'index.html'), encoding)
-    html = html.replace(/{%TITLE%}/, `Not found: ${basename(currentFile)}`).replace(/{%HOST%}/g, host).replace(/{%PORT%}/g, port)
-    res.send(html)
+    res.header('Content-Type', 'text/html; charset=' + encoding);
+    let html = readFileSync(resolve(__dirname, 'index.html'), encoding);
+    html = html.replace(/{%TITLE%}/, `Not found: ${basename(currentFile)}`).replace(/{%HOST%}/g, host).replace(/{%PORT%}/g, port);
+    res.send(html);
   }
-})
+});
 
-const server = require('http').createServer(app)
-const io = require('socket.io')(server)
+const server = require('http').createServer(app);
+const io = require('socket.io')(server);
 
 io.on('connection', (socket) => {
   const responseContent = async () => {
-    let content
-    const text = existsCurrentFile ? readFileSync(currentFile, encoding) : `Not found: ${currentFile}`
-    const markdownExtension = /\.m(arkdown|kdn?|d(o?wn)?)(\?.*)?(#.*)?$/i
+    let content;
+    const text = existsCurrentFile ? readFileSync(currentFile, encoding) : `Not found: ${currentFile}`;
+    const markdownExtension = /\.m(arkdown|kdn?|d(o?wn)?)(\?.*)?(#.*)?$/i;
     if (markdownExtension.test(extname(currentFile))) {
       try {
-        const response = await axios.post(apiUrl + '/markdown', { text, mode: 'gfm' })
-        content = response.data
+        const response = await axios.post(apiUrl + '/markdown', { text, mode: 'gfm' });
+        content = response.data;
       } catch (e) {
-        content = e.response.data.message
+        content = e.response.data.message;
       }
     } else {
-      content = `<pre><code>${text}</code></pre>`
+      content = `<pre><code>${text}</code></pre>`;
     }
-    socket.emit('response content', content)
+    socket.emit('response content', content);
   }
 
   watcher.on('change', () => {
-    console.log(chalk`> {cyan gfm-preview}: Detect changes and reloaded content (${basename(currentFile)})`)
-    responseContent()
-  })
+    console.log(chalk`> {cyan gfm-preview}: Detect changes and reloaded content (${basename(currentFile)})`);
+    responseContent();
+  });
 
   socket.on('request content', () => {
-    responseContent()
-  })
+    responseContent();
+  });
 
   socket.on('clicking', () => {
-    clicking = true
-    socket.emit('clickable')
-  })
+    clicking = true;
+    socket.emit('clickable');
+  });
 
   socket.on('clicked', () => {
-    clicking = false
-  })
+    clicking = false;
+  });
 
   socket.on('disconnect', () => {
     if (!clicking && io.sockets.server.engine.clientsCount === 0) {
-      console.log(chalk`> {cyan gfm-preview}: Have a nice code!`)
-      process.exit(0)
+      console.log(chalk`> {cyan gfm-preview}: Have a nice code!`);
+      process.exit(0);
     }
-  })
-})
+  });
+});
 
-const host = args['--host'] ? args['--host'] : 'localhost'
-const url = `http://${host}:${port}`
+const host = args['--host'] ? args['--host'] : 'localhost';
+const url = `http://${host}:${port}`;
 const start = async () => {
   try {
     if (args['--browser']) {
-      require('open')(url + '/' + basename(currentFile), { app: args['--browser'] })
+      require('open')(url + '/' + basename(currentFile), { app: args['--browser'] });
     } else {
-      require('open')(url + '/' + basename(currentFile))
+      require('open')(url + '/' + basename(currentFile));
     }
     await server.listen(port, host, () => {
-      console.log(chalk`> {cyan gfm-preview}: Ready on ${url}`)
-    })
+      console.log(chalk`> {cyan gfm-preview}: Ready on ${url}`);
+    });
   } catch (err) {
-    console.error(err)
-    process.exit(1)
+    console.error(err);
+    process.exit(1);
   }
 }
 
-start()
+start();
